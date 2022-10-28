5a38ee93c10813edbc673219f46beb8006a88744
Stefanus Terang <79499530+TerangStefanus@users.noreply.github.com>
2022-10-28 23:29:57 +0700

    Update UI Menu Scene
    Minta tolong mas Tian setting lagi soale pas ngeplay cacat geming ya ges ya , ada tambahan Scene Menu bisa dicoba

---
+++ Assets/Global Script/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+        // This code do load Scene based on build setting + 1
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit !!!");
+        Application.Quit();
+    }
+
+
+}
